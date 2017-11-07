9f79a6e500b8af59c5929b09e633ebd178c24697
jarec <ormus395@gmail.com>
2017-11-07 03:11:28 -0700

    changed prod page

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+if(process.env.NODE_ENV === 'production') {
+  app.send('<h1>Site is under construction</h1>')
+}
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, (req, res) => {
